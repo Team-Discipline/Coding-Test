c028cf6c3d5c9e7fc60ad2faa3c4bc7221844935
Aksidion Kreimben <aksidion@kreimben.com>
2022-11-17 16:03:09 +0900

    혼자풀기 신공

--- 20221114/Find Closest Value.py
+++ 20221114/Find Closest Value.py
@@ -12,32 +12,40 @@ class BST:
         self.right = None
 
 
-def bst(tree: BST, target: int, result: [int]) -> bool:
-    # v = tree.value
-    # t = target
-    if len(result):
-        return False
-    elif tree is None:
-        return True
-    elif tree.value == target:
-        result.append(tree.value)
-        return False
-    elif tree.value >= target:
-        if bst(tree.left, target, result):
-            result.append(tree.value)
-        return False
-    else:
-        if bst(tree.right, target, result):
-            result.append(tree.value)
-        return False
+"""
+The solution must return closest value!!!
+"""
+
+
+def bfs(tree: BST, target: int, result: [int]):
+    if tree is not None:
+        a = abs(tree.value - target)
+        if a < abs(result[0] - target):
+            result[0] = tree.value
+
+        if tree.left is not None:
+            bfs(tree.left, target, result)
+
+        if tree.right is not None:
+            bfs(tree.right, target, result)
 
 
 def findClosestValueInBst(tree: BST, target: int):
-    result = []
-    bst(tree, target, result)
-    print(f'{result[0]=}')
+    result = [float('INF')]
+    bfs(tree, target, result)
     return result[0]
 
+
+root = BST(10)
+root.left = BST(5)
+root.left.left = BST(2)
+root.left.left.left = BST(1)
+root.left.right = BST(5)
+root.right = BST(15)
+root.right.left = BST(13)
+root.right.left.right = BST(14)
+root.right.right = BST(22)
+assert findClosestValueInBst(root, 12) == 13
 
 root = BST(100)
 root.left = BST(5)
@@ -53,4 +61,5 @@ root.left.right.right = BST(22)
 root.left.left.left.left = BST(-51)
 root.left.left.left.right = BST(1)
 root.left.left.left.left.left = BST(-403)
+assert findClosestValueInBst(root, 100) == 100
 assert findClosestValueInBst(root, -70) == -51
