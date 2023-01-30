d9f3345f43702aa601ea59fed57b58e9ac367179
Aksidion Kreimben <aksidion@kreimben.com>
2023-01-30 18:24:03 +0900

    혼자풀기 신공

--- leetcode.com/Two Sum.py
+++ leetcode.com/Two Sum.py
@@ -2,35 +2,26 @@
 https://leetcode.com/problems/two-sum/
 Two Sum
 """
-from itertools import combinations
-
-
-class Num:
-    def __init__(self, value: int, index: int):
-        self.value = value
-        self.index = index
-
-    def __repr__(self):
-        return f'{self.value}({self.index})'
 
 
 class Solution:
     def twoSum(self, nums: [int], target: int) -> [int]:
-        n = []
-        for i, v in enumerate(nums):
-            n.append(Num(v, i))
-        n.sort(key=lambda x: x.value)
-        c = combinations(n, 2)
+        d = {}
+        for i in range(len(nums)):
+            num = nums[i]
+            d[target - num] = i
 
-        for i in c:
-            if i[0].value + i[1].value == target:
-                return [i[0].index, i[1].index]
+        for wanted, target_index in d.items():
+            if wanted in nums:
+                wanted_index = nums.index(wanted)
+                if target_index != wanted_index:
+                    return sorted([target_index, wanted_index])
 
 
 s = Solution()
 
-print(s.twoSum([-3, 4, 3, 90], 0) == [0, 2])
-print(s.twoSum([2, 3, 4], 6) == [0, 2])
-print(s.twoSum([3, 3], 6) == [0, 1])
-print(s.twoSum([0, 4, 3, 0], 0) == [0, 3])
-print(s.twoSum([-3, 4, 3, 90], 0) == [0, 2])
+assert s.twoSum([-3, 4, 3, 90], 0) == [0, 2]
+assert s.twoSum([2, 3, 4], 6) == [0, 2]
+assert s.twoSum([3, 3], 6) == [0, 1]
+assert s.twoSum([0, 4, 3, 0], 0) == [0, 3]
+assert s.twoSum([-3, 4, 3, 90], 0) == [0, 2]
