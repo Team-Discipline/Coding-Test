05fffc6fa89157315432206a85019a21621fedef
Aksidion Kreimben <aksidion@kreimben.com>
2023-03-20 11:27:00 +0900

    혼자풀기 신공

--- leetcode.com/Minimum Window Substring.py
+++ leetcode.com/Minimum Window Substring.py
@@ -2,51 +2,101 @@
 https://leetcode.com/problems/minimum-window-substring/
 Minimum Window Substring
 """
+from unittest import TestCase
 
 
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if len(s) < len(t):
-            return ''
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        # Struggled with this problem for a long while.
+        # Idea: Two pointers: moving end forward to find a valid window,
+        #                     moving start forward to find a smaller window
+        #                     counter and hash_map to determine if the window is valid or not
 
+        # Count the frequencies for chars in t
+        hash_map = dict()
         for ch in t:
-            if ch not in s:
-                return ''
+            hash_map[ch] = 1 + hash_map.get(ch, 0)
 
-        minval = str(s)
+        start, end = 0, 0
 
-        # dt = {}
-        # for ch in t:
-        #     dt[ch] = 1 + dt.get(ch, 0)
+        # If the minimal length doesn't change, it means there's no valid window
+        min_window_length = len(s) + 1  # To check initial window is changed.
 
-        have, need = 0, len(t)
-        left = 0
-        for right, ch in enumerate(s):
-            have += 1
-            block = s[left: right + 1]
-            if s[right] in t:
-                need -= 1
+        # Start point of the minimal window
+        min_window_start = 0
 
-            if need == 0:
-                if len(minval) > (right - left + 1):
-                    minval = s[left: right + 1]
-                    left += 1
-                    have -= 1
-                    need += 1
+        # Works as a counter of how many chars still need to be included in a window
+        num_of_chars_to_be_included = len(t)
 
-            while s[left] not in t and left <= right:
-                block = s[left: right + 1]
-                have -= 1
-                left += 1
+        while end < len(s):
+            # If the current char is desired
+            if s[end] in hash_map:
+                # Then we decreased the counter, if this char is a "must-have" now, in a sense of critical value
+                if hash_map[s[end]] > 0:
+                    num_of_chars_to_be_included -= 1
+                # And we decrease the hash_map value
+                hash_map[s[end]] -= 1
 
-        return minval
+            # If the current window has all the desired chars
+            while num_of_chars_to_be_included == 0:
+                # See if this window is smaller
+                if end - start + 1 < min_window_length:
+                    min_window_length = end - start + 1
+                    min_window_start = start
+
+                # if s[start] is desired, we need to update the hash_map value and the counter
+                if s[start] in hash_map:
+                    hash_map[s[start]] += 1
+                    # Still, update the counter only if the current char is "critical"
+                    if hash_map[s[start]] > 0:
+                        num_of_chars_to_be_included += 1
+
+                # Move start forward to find a smaller window
+                start += 1
+
+            # Move end forward to find another valid window
+            end += 1
+
+        if min_window_length == len(s) + 1:
+            return ""
+        else:
+            return s[min_window_start:min_window_start + min_window_length]
 
 
-s = Solution()
-# actual = s.minWindow('ab', 'b')
-# print(f'{actual=}')
-# assert actual == 'b'
+class Case(TestCase):
+    s = Solution()
 
-actual = s.minWindow("ADOBECODEBANC", "ABC")
-print(f'{actual=}')
-assert actual == 'BANC'
+    def test1(self):
+        actual = self.s.minWindow("ADOBECODEBANC", "ABC")
+        expected = 'BANC'
+        self.assertEqual(actual, expected)
+
+    def test2(self):
+        actual = self.s.minWindow('ab', 'b')
+        expected = 'b'
+        self.assertEqual(actual, expected)
+
+    def test3(self):
+        actual = self.s.minWindow('a', 'aa')
+        exected = ''
+        self.assertEqual(actual, exected)
+
+    def test4(self):
+        actual = self.s.minWindow('a', 'a')
+        expected = 'a'
+        self.assertEqual(actual, expected)
+
+    def test5(self):
+        actual = self.s.minWindow('a', 'b')
+        expected = ''
+        self.assertEqual(actual, expected)
+
+    def test6(self):
+        actual = self.s.minWindow('bba', 'ba')
+        expected = 'ba'
+        self.assertEqual(actual, expected)
