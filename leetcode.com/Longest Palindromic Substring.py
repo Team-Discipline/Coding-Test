83a25ce1e7784ea26074c755fcb7e29acacea59e
Aksidion Kreimben <aksidion@kreimben.com>
2023-03-23 00:01:14 +0900

    혼자풀기 신공

--- leetcode.com/Longest Palindromic Substring.py
+++ leetcode.com/Longest Palindromic Substring.py
@@ -6,34 +6,38 @@ Longest Palindromic Substring
 
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        maxval = ''
-        for i in range(len(s)):
-            if len(maxval) > i * 2:
-                continue
+        dp = ['' for _ in range(len(s) + 1)]
+        # None b ba bab baba babad (dp table)
+        #  ''  b  b bab bab   bab  (lognest palindrom)
+        for i in range(1, len(dp)):
+            def find(left: int, right: int):
+                nonlocal s, i
+                if dp[i] != '':
+                    return
+                while left <= right:
+                    if (right - left + 1) <= len(dp[i - 1]):
+                        break
+                    b = s[left: right + 1]
+                    if b == b[::-1]:
+                        # Don't need to find furthur.
+                        dp[i] = b
+                        break
+                    find(left + 1, right)
+                    find(left, right - 1)
+                    break
+                if dp[i] == '':
+                    dp[i] = dp[i - 1]
 
-            # odd case
-            append = len(maxval) // 2
-            while -1 < i - append and i + append < len(s):
-                block = s[i - append: i] + s[i] + s[i + 1: i + append + 1]
-                if block == block[::-1]:
-                    maxval = block
-                append += 1
+            find(0, i - 1)
 
-            # even case
-            append = len(maxval) // 2
-            while -1 < i - append and i + append + 1 < len(s):
-                block = s[i - append: i + 2] + s[i + 2: i + append + 2]
-                if block == block[::-1]:
-                    maxval = block
-                append += 1
-        return maxval
+        return dp[-1]
 
 
 s = Solution()
+assert s.longestPalindrome("babad") == 'bab'
 assert s.longestPalindrome("abb") == 'bb'
 assert s.longestPalindrome("cbbd") == 'bb'
 assert s.longestPalindrome('aacabdkacaa') == 'aca'
 assert s.longestPalindrome("a") == "a"
 assert s.longestPalindrome('ac') == 'a'
 assert s.longestPalindrome("bb") == 'bb'
-assert s.longestPalindrome("babad") == 'bab'
