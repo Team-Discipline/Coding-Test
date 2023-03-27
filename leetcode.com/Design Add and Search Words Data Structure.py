bad0284f8321559cbb65d8d1fdb70c7c15366ff7
Aksidion Kreimben <aksidion@kreimben.com>
2023-03-27 23:33:19 +0900

    혼자풀기 신공

--- leetcode.com/Design Add and Search Words Data Structure.py
+++ leetcode.com/Design Add and Search Words Data Structure.py
@@ -5,47 +5,82 @@ Design Add and Search Words Data Structure
 from unittest import TestCase
 
 
+# class WordDictionary:
+#     """
+#     It's not very efficient code.
+#     That's reason to use `trie`.
+#     """
+#     def __init__(self):
+#         self.d = defaultdict(set)
+#
+#     def addWord(self, word: str) -> None:
+#         self.d[len(word)].add(word)
+#
+#     def search(self, word: str) -> bool:
+#         m = len(word)
+#         for dict_word in self.d[m]:
+#             i = 0
+#             while i < m and (dict_word[i] == word[i] or word[i] == '.'):
+#                 i += 1
+#             if i == m:
+#                 return True
+#         return False
 class WordDictionary:
 
     def __init__(self):
-        self.d = {}
+        """
+        Initialize your data structure here.
+        """
+        self.trie = {}
 
     def addWord(self, word: str) -> None:
-        self.d[word] = True
+        """
+        Adds a word into the data structure.
+        """
+        node = self.trie
+
+        for ch in word:
+            if not ch in node:
+                node[ch] = {}
+            node = node[ch]
+        node['$'] = True
 
     def search(self, word: str) -> bool:
-        ready = list(filter(lambda x: len(x) == len(word), self.d.keys()))
-        if not ready:
-            return False
+        def search_in_node(word, node) -> bool:
+            for i, ch in enumerate(word):
+                if not ch in node:
+                    # if the current character is '.'
+                    # check all possible nodes at this level
+                    if ch == '.':
+                        for x in node:
+                            if x != '$' and search_in_node(word[i + 1:], node[x]):
+                                return True
+                    # if no nodes lead to answer
+                    # or the current character != '.'
+                    return False
+                # if the character is found
+                # go down to the next level in trie
+                else:
+                    node = node[ch]
+            return '$' in node
 
-        valid_chars = []
-        for i, ch in enumerate(word):
-            if ch != '.':
-                valid_chars.append(i)
-
-        if len(valid_chars) != len(word):
-            for can_index, candidate in enumerate(ready):
-                for i, ch in enumerate(candidate):
-                    if i not in valid_chars:
-                        ready[can_index] = ready[can_index][:i] + '.' + ready[can_index][i + 1:]
-
-        if word in ready:
-            return True
-        else:
-            return False
+        return search_in_node(word, self.trie)
 
 
 class Case(TestCase):
 
-    # def test1(self):
-    #     commands = ["WordDictionary", "addWord", "addWord", "addWord", "addWord", "search", "search", "addWord",
-    #                 "search", "search", "search", "search", "search", "search"]
-    #     inputs = [[], ["at"], ["and"], ["an"], ["add"], ["a"], [".at"], ["bat"], [".at"], ["an."], ["a.d."], ["b."],
-    #               ["a.d"], ["."]]
-    #
-    #     actual = self.run_with(commands, inputs)
-    #     expected = [None, None, None, None, None, False, False, None, True, True, False, False, True, False]
-    #     self.assertEqual(actual, expected)
+    def test1(self):
+        commands = ["WordDictionary", "addWord", "addWord", "addWord", "addWord", "search", "search", "addWord",
+                    "search", "search", "search", "search", "search", "search"]
+        inputs = [[], ["at"], ["and"], ["an"], ["add"], ["a"], [".at"], ["bat"], [".at"], ["an."], ["a.d."], ["b."],
+                  ["a.d"], ["."]]
+
+        actual = self.run_with(commands, inputs)
+        expected = [None, None, None, None, None, False, False, None, True, True, False, False, True, False]
+        # for command, i, a, e in list(zip(commands, inputs, actual, expected)):
+        #     if command == 'search' and a != e:
+        #         print(f'{command=} {i=} {a=} {e=}')
+        self.assertEqual(actual, expected)
 
     def test2(self):
         commands = ["WordDictionary", "addWord", "addWord", "addWord", "search", "search", "search", "search"]
