172c1ec1232b97b376bec3f50e69d296b54207a5
Aksidion Kreimben <aksidion@kreimben.com>
2023-05-17 22:04:46 +0900

    혼자풀기 신공

--- leetcode.com/Reverse Linked List.py
+++ leetcode.com/Reverse Linked List.py
@@ -2,6 +2,7 @@
 https://leetcode.com/problems/reverse-linked-list/
 Reverse Linked List
 """
+from typing import Optional
 
 
 # Definition for singly-linked list.
@@ -12,29 +13,12 @@ class ListNode:
 
 
 class Solution:
-    def reverseList(self, head: [ListNode]) -> [ListNode]:
-        stack = []
-        result = None
-
-        while head is not None:
-            stack.append(head.val)  # 1, 2, 3, 4, 5
-            head = head.next
-
-        while stack:
-            temp = ListNode(val=stack.pop())  # 5, 4, 3, 2, 1
-            if result is None:
-                result = temp
-            else:
-                t = result
-                while t.next is not None:
-                    t = t.next
-                t.next = temp
-
-        return result
-
-
-s = Solution()
-assert s.reverseList(
-    ListNode(val=1, next=ListNode(val=2, next=ListNode(val=3, next=ListNode(val=4, next=ListNode(val=5, next=None)))))) \
-       == ListNode(val=5,
-                   next=ListNode(val=4, next=ListNode(val=3, next=ListNode(val=2, next=ListNode(val=1, next=None)))))
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        curr = head
+        prev = None
+        while curr:
+            next = curr.next  # back up next element for next loop.
+            curr.next = prev  # reverse current elements.
+            prev = curr  # set current node as prev. i will go to next node.
+            curr = next  # go to next node!
+        return prev
